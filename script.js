c37089976bf958149bd617d72f6a299023e4a2de
Choua Vue <vuec23@gmail.com>
2022-03-09 19:06:06 -0500

    updating js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent = 'Things you like about my page';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
